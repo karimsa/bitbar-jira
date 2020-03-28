3cda1fe7e6c67ea2059f8c455b0fad66cbb1829c
Karim Alibhai <karim@alibhai.co>
2020-03-28 15:27:11 -0400

    Add: transition buttons

--- list-issues.sh
+++ list-issues.sh
@@ -28,13 +28,52 @@ function ls_issues() {
 	curl \
 		-fsSL \
 		-u "${JIRA_USERNAME}:${JIRA_API_KEY}" \
-		https://${JIRA_NAMESPACE}.atlassian.net/rest/api/3/search\?jql\=`echo "project = ${JIRA_PROJECT} AND resolution IS EMPTY AND status = '${1}' ORDER BY updated DESC" | sed 's/ /%20/'g`
+		https://${JIRA_NAMESPACE}.atlassian.net/rest/api/3/search\?jql\=`echo "project = ${JIRA_PROJECT} AND (fixVersion IS EMPTY OR fixVersion IN unreleasedVersions()) AND status = '${1}' ORDER BY updated DESC" | sed 's/ /%20/'g` \
+		| jq -c '.issues[]' \
+		| while read row; do
+		echo "$row" | jq -r "\":$2: [\(.key)] \(.fields.summary) (\(.duedate)) | href=https://${JIRA_NAMESPACE}.atlassian.net/browse/\(.key)\""
+
+		echo "Backlog,Selected for Development,In Progress,Done," | while read -d, status; do
+			echo "--Move to $status | bash=$HOME/.bitbar-jira.sh param1=$JIRA_NAMESPACE param2=`echo "$row" | jq -r .key` param3=$JIRA_USERNAME param4=$JIRA_API_KEY param5='$status' terminal=true"
+		done
+	done
 }
 
-ls_issues "Selected for Development" | jq -r ".issues[] | \":memo: [\(.key)] \(.fields.summary) (\(.duedate)) | href=https://${JIRA_NAMESPACE}.atlassian.net/browse/\(.key)\""
-ls_issues "In Progress" | jq -r ".issues[] | \":construction: [\(.key)] \(.fields.summary) (\(.duedate)) | href=https://${JIRA_NAMESPACE}.atlassian.net/browse/\(.key)\""
-ls_issues "Done" | jq -r ".issues[] | \":white_check_mark: [\(.key)] \(.fields.summary) (\(.duedate)) | href=https://${JIRA_NAMESPACE}.atlassian.net/browse/\(.key)\""
+cat > ~/.bitbar-jira.sh << _EOF
+#!/bin/bash
+set -e
+set -o pipefail
+
+JIRA_NAMESPACE=\$1
+JIRA_ISSUE_KEY=\$2
+JIRA_USERNAME=\$3
+JIRA_API_KEY=\$4
+JIRA_TRANSITION=\$5
+
+transitionID=\`curl -fsSL -u "\${JIRA_USERNAME}:\${JIRA_API_KEY}" https://${JIRA_NAMESPACE}.atlassian.net/rest/api/3/issue/\$JIRA_ISSUE_KEY/transitions | jq -r ".transitions[] | select(.name == \"\$JIRA_TRANSITION\") | .id"\`
+if test -z "\$transitionID"; then
+	echo "No such transition exists."
+	exit 1
+fi
+
+curl \
+	-fsSL \
+	-u "\${JIRA_USERNAME}:\${JIRA_API_KEY}" \
+	-X POST \
+	-H 'Content-Type: application/json' \
+	--data "{\"transition\":{\"id\":\"\$transitionID\"}}" \
+	https://${JIRA_NAMESPACE}.atlassian.net/rest/api/3/issue/\$JIRA_ISSUE_KEY/transitions
+open -g "bitbar://refreshPlugin?name=jira*.*.sh"
+
+_EOF
+chmod +x ~/.bitbar-jira.sh
+
+ls_issues "Selected for Development" "memo"
+ls_issues "In Progress" "construction"
+ls_issues "Done" "white_check_mark"
 
 echo "---"
 boardID=`curl -fsSL -u "${JIRA_USERNAME}:${JIRA_API_KEY}" https://${JIRA_NAMESPACE}.atlassian.net/rest/agile/1.0/board | jq -r ".values[] | select(.location.projectKey == \"${JIRA_PROJECT}\") | .id"`
 echo "Visit board | href=https://${JIRA_NAMESPACE}.atlassian.net/secure/RapidBoard.jspa?rapidView=${boardID}"
+echo "Releases | href=https://${JIRA_NAMESPACE}.atlassian.net/projects/${JIRA_PROJECT}?selectedItem=com.atlassian.jira.jira-projects-plugin:release-page"
+echo "Refresh Issues | terminal=false refresh=true"
